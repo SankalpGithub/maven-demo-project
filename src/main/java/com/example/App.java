d41028241eea6df87614708a7bca4fb3727b4fda
Sankalp Gaikwad <sankalp2004gaikwad@gmail.com>
2026-02-27 11:44:58 +0530

    Fix syntax error in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,7 +7,7 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println("Maven + Jenkins is working!")
+        System.out.println("Maven + Jenkins is working!");
         System.out.println("Sum: " + add(5, 3));
     }
 }
